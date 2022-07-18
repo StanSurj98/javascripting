fb14e4f794f879f6a78d4840423187fc18f3c274
Stanley Surjanto <stan.surjanto@gmail.com>
2022-07-18 00:35:50 -0600

    Initial commit of all my work from the Javascripting workshop module

---
+++ function-arguments.js
@@ -0,0 +1,17 @@
+function math (argOne, argTwo, argThree) {
+  let product = argTwo * argThree;
+  return product + argOne;
+};
+
+console.log(math(53, 61, 67));
+
+/* 
+
+or you can also do 
+
+function math (a, b, c) {
+  return (b * c) + a;
+};
+console.log(math(53, 61, 67));
+
+*/
